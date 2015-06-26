d52e59dcbce8371515d2d6b4adcef219b7ab95fa
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-06-26 12:14:39 +0100

    Adds background space.

--- Main.elm
+++ Main.elm
@@ -8,37 +8,30 @@ import Window
 import Debug
 
 type alias Keys = { x:Int, y:Int }
-  
+
 type alias Model =
   { angle : Float
   , velocity : Float
   }
-  
+
 spaceship : Model
 spaceship =
   { angle = 0.0
   , velocity = 0.0
   }
 
-spaceshipSprite : Shape
-spaceshipSprite =
-  ngon 3 20
-
-clearGrey : Color
-clearGrey =
-  rgba 111 111 111 0.6
-       
-toDrawableAngle : Float -> Float
-toDrawableAngle shipAngle =
-  degrees -shipAngle * 3
-
-keyboardInput : Signal (Float, Keys)
-keyboardInput =
+spaceSprite : Int -> Int -> Form
+spaceSprite w' h' =
   let
-    delta = Signal.map (\t -> t/2) (fps 60)
+    w = toFloat(w')
+    h = toFloat(h')
   in
-    Signal.sampleOn delta (Signal.map2 (,) delta Keyboard.arrows)
-          
+    filled black (rect w h)
+
+spaceshipSprite : Form
+spaceshipSprite =
+  outlined (solid white) (ngon 3 20)
+
 updateSpaceship : (Float, Keys) -> Model -> Model
 updateSpaceship (time, keys) ship =
   let
@@ -46,21 +39,29 @@ updateSpaceship (time, keys) ship =
     newVelocity = ship.velocity + 0.1
   in
     { ship | angle <- newAngle, velocity <- newVelocity }
-    
-drawShip : Model -> Element
-drawShip ship =
+
+drawScene : (Int, Int) -> Model -> Element
+drawScene (w, h) ship =
   let
     dbg = Debug.watch "ship" ship
   in
-    collage 300 300
-        [ spaceshipSprite
-        |> filled clearGrey
-        |> rotate (toDrawableAngle ship.angle) ]
-          
+    collage w h
+      [ spaceSprite w h
+      , spaceshipSprite
+        |> rotate (degrees -ship.angle * 3)
+      ]
+
+keyboardInput : Signal (Float, Keys)
+keyboardInput =
+  let
+    delta = Signal.map (\t -> t/2) (fps 60)
+  in
+    Signal.sampleOn delta (Signal.map2 (,) delta Keyboard.arrows)
+
 spaceshipInput : Signal Model
 spaceshipInput =
   Signal.foldp updateSpaceship spaceship keyboardInput
 
 main : Signal Element
 main =
-  Signal.map drawShip spaceshipInput
+  Signal.map2 drawScene Window.dimensions spaceshipInput
