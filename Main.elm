264b8a59281e9699fd52ae1e434335721a48c069
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-06-26 11:51:09 +0100

    Triangle that rotates when pressing left/right.

--- Main.elm
+++ Main.elm
@@ -1,18 +1,23 @@
 import Graphics.Element exposing (..)
 import Graphics.Collage exposing (..)
+import Time exposing (..)
 import Color exposing (..)
 import Mouse
+import Keyboard
 import Window
+import Debug
 
+type alias Keys = { x:Int, y:Int }
+  
 type alias Model =
-  { x : Float
-  , y : Float
+  { angle : Float
+  , velocity : Float
   }
   
 spaceship : Model
 spaceship =
-  { x = 0
-  , y = 0
+  { angle = 0.0
+  , velocity = 0.0
   }
 
 spaceshipSprite : Shape
@@ -22,18 +27,40 @@ spaceshipSprite =
 clearGrey : Color
 clearGrey =
   rgba 111 111 111 0.6
+       
+toDrawableAngle : Float -> Float
+toDrawableAngle shipAngle =
+  degrees -shipAngle * 3
 
-draw : Model -> (Int, Int) -> Form
-draw spaceship (x,y) =
-  spaceshipSprite
-  |> filled clearGrey
-  |> move(toFloat(x),toFloat(-y))
-
-scene : (Int, Int) -> (Int, Int) -> Element
-scene (left,top) shipPosition =
-  collage left top
-    [ draw spaceship shipPosition]
+keyboardInput : Signal (Float, Keys)
+keyboardInput =
+  let
+    delta = Signal.map (\t -> t/2) (fps 60)
+  in
+    Signal.sampleOn delta (Signal.map2 (,) delta Keyboard.arrows)
+          
+updateSpaceship : (Float, Keys) -> Model -> Model
+updateSpaceship (time, keys) ship =
+  let
+    newAngle = ship.angle + toFloat(keys.x)
+    newVelocity = ship.velocity + 0.1
+  in
+    { ship | angle <- newAngle, velocity <- newVelocity }
+    
+drawShip : Model -> Element
+drawShip ship =
+  let
+    dbg = Debug.watch "ship" ship
+  in
+    collage 300 300
+        [ ngon 3 20
+        |> filled clearGrey
+        |> rotate (toDrawableAngle ship.angle) ]
+          
+spaceshipInput : Signal Model
+spaceshipInput =
+  Signal.foldp updateSpaceship spaceship keyboardInput
 
 main : Signal Element
 main =
-  Signal.map2 scene Window.dimensions Mouse.position
+  Signal.map drawShip spaceshipInput
